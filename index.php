61b75df78b280bd93d9dc8c8e9149f2b57241752
vallaria <vallariant@gmail.com>
2022-05-09 14:24:45 +0900

    add 各種ファイルの追加
    共通部品及びトップページ関連のファイルを追加しました。

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="format-detection" content="telephone=no,address=no,email=no">
+<meta property="description" content="異世界スマホの世界をまるごと解説！人気急上昇作品の世界もこれで丸わかり！ネタバレ防止機能付き">
+<meta property="og:site_name" content="異世界はスマートフォンとともに。応援サイト">
+<meta property="og:url" content="http://isesuma-data.com/">
+<meta property="og:title" content="異世界はスマートフォンとともに。応援サイト">
+<meta property="og:description" content="異世界スマホの世界をまるごと解説！人気急上昇作品の世界もこれで丸わかり！">
+<meta property="og:type" content="website">
+<!-- 要修正 <meta property="og:image" content="./img/paula-icon.jpg"> --?
+<!-- 要修正 <link rel="canonical" href="http://vallaria.net/">　-->
+<!-- bootstrap -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+<!-- googleFont -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@400;500&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+<!-- costum CSS -->
+<link rel="stylesheet" href="/css/common.css">
+<link rel="stylesheet" href="/css/top.css">
+<title>異世界はスマートフォンとともに。応援サイト</title>
+<body>
+  <header>
+    <h1>
+      <img src="/img/common/logo.png">
+    </h1>
+    <nav class="gnav">
+      <ul>
+        <li class="gnav_item">トップページ</li>
+        <li class="gnav_item">キャラ相関図</li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <section id="topBar" class="fs-4">異世界はスマートフォンとともに。応援サイト</section>
+    <section id="topImg">
+      <img class="mx-auto d-block" src="/img/top/main_img.jpg">
+    </section>
+    <section id="news" class="wave">
+      <h3 class="text-center">
+        <span>お知らせ</span>
+      </h3>
+      <table id="news_table" class="table my-4">
+        <tbody>
+          <tr>
+            <th>11月11日 11:11</th>
+            <td>「キャラ相関図」を更新しました。</td>
+          </tr>
+          <tr>
+            <th>11月11日 11:11</th>
+            <td>「トップページ」を更新しました。</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <section id="link" class="bg-cyan">
+      <h3 class="text-center">
+        <span>リンク集</span>
+      </h3>
+      <table id="news_table" class="table table-borderless my-4">
+        <tbody>
+          <tr>
+            <th class="text-end"><span class="badge-costom">原作Web小説</span></th>
+            <td><a href="https://ncode.syosetu.com/n1443bp/">異世界はスマートフォンとともに。 - 小説家になろう</a></td>
+          </tr>
+          <tr>
+            <th class="text-end"><span class="badge-costom">ライトノベル</span></th>
+            <td><a href="https://firecross.jp/hjnovels/series/224">異世界はスマートフォンとともに。｜HJノベルス公式Webサイト</a></td>
+          </tr>
+          <tr>
+            <th class="text-end"><span class="badge-costom">コミック</span></th>
+            <td><a href="http://comic-walker.com/contents/detail/KDCW_KS04000031010000_68/">異世界はスマートフォンとともに。｜ComicWalker コミックウォーカー</a></td>
+          </tr>
+          <tr>
+            <th class="text-end"><span class="badge-costom">アニメ</span></th>
+            <td><a href="http://isesuma-anime.jp/">TVアニメ『異世界はスマートフォンとともに。』公式サイト</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  </main>
+  <footer>
+    <span>異世界はスマートフォンとともに。応援サイト</span>
+  </footer>
+</body>
+</html>
