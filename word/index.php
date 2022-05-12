747f0012972739281fd9e356e721a8aae15f6f16
vallaria <vallariant@gmail.com>
2022-05-12 22:32:56 +0900

    add word追加
    用語一覧ページ追加、modallist用cssファイル名変更

--- word/index.php
+++ word/index.php
@@ -22,64 +22,83 @@ function translateTypeJPtoEN($jp){
   };
 }
 $pdo = getPdo();
-if(!$pdo == null){
-  if(isset($_POST['subtype']) && $_POST['subtype'] == "reset"){
-    $_POST['target'] = null;
-    $_POST['search'] = null;
-    $_POST['compare'] = null;
-    $_POST['order'] = null;
-  }
-  if(isset($_POST['search'])){
-    if($_POST['compare'] == "equal"){
-      $sql_compare = "=";
-    }else{
-      $sql_compare = "LIKE";
-    }
-    if($_POST['target'] == 'name'){
-      $sql = "SELECT * FROM isesuma_magic WHERE name " . $sql_compare . " :search";
-    }else{
-      $sql = "SELECT * FROM isesuma_magic WHERE name " . $sql_compare . " :search OR chant " . $sql_compare . " :search OR detail " . $sql_compare . " :search";
-    }
-  }else{
-    $sql = "SELECT * FROM isesuma_magic";
-  }
-  if(isset($_POST['order'])){
-    if($_POST['order'] == "yomi"){
-      $sql .= " ORDER BY name";
-    }else if($_POST['order'] == "appear"){
-      $sql .= " ORDER BY id";
-    }else if($_POST['order'] == "type"){
-      $sql .= " ORDER BY type, name";
-    }
-  }else{
-    $sql .= " ORDER BY name";
-  }
-  $stmt = $pdo->prepare($sql);
-  if(isset($_POST['search'])){
-    if($_POST['compare'] == "equal"){
-      $bindstr = $_POST['search'];
-    }else{
-      $bindstr = "%" . $_POST['search'] . "%";
-    }
-    $stmt->bindValue(':search', $bindstr);
-  }
-  $stmt->execute();
-  $magicList = array();
-  foreach ($stmt as $row){
-    // 配列の順
-    // (0)識別ID, (1)表示制限, (2)登場媒体, (3)名前, (4)属性, (5)属性英語名, (6)詠唱, (7)説明 
-    $magicList[] = array(
-      $row['id']
-      , $row['netagard']
-      , $row['media']
-      , $row['name']
-      , $row['type']
-      , translateTypeJPtoEN($row['type'])
-      , $row['chant']
-      , $row['detail']
-    );
-  }
-}?>
+// if(!$pdo == null){
+//   if(isset($_POST['subtype']) && $_POST['subtype'] == "reset"){
+//     $_POST['target'] = null;
+//     $_POST['search'] = null;
+//     $_POST['compare'] = null;
+//     $_POST['order'] = null;
+//   }
+//   if(isset($_POST['search'])){
+//     if($_POST['compare'] == "equal"){
+//       $sql_compare = "=";
+//     }else{
+//       $sql_compare = "LIKE";
+//     }
+//     if($_POST['target'] == 'name'){
+//       $sql = "SELECT * FROM isesuma_magic WHERE name " . $sql_compare . " :search";
+//     }else{
+//       $sql = "SELECT * FROM isesuma_magic WHERE name " . $sql_compare . " :search OR chant " . $sql_compare . " :search OR detail " . $sql_compare . " :search";
+//     }
+//   }else{
+//     $sql = "SELECT * FROM isesuma_magic";
+//   }
+//   if(isset($_POST['order'])){
+//     if($_POST['order'] == "yomi"){
+//       $sql .= " ORDER BY name";
+//     }else if($_POST['order'] == "appear"){
+//       $sql .= " ORDER BY id";
+//     }else if($_POST['order'] == "type"){
+//       $sql .= " ORDER BY type, name";
+//     }
+//   }else{
+//     $sql .= " ORDER BY name";
+//   }
+//   $stmt = $pdo->prepare($sql);
+//   if(isset($_POST['search'])){
+//     if($_POST['compare'] == "equal"){
+//       $bindstr = $_POST['search'];
+//     }else{
+//       $bindstr = "%" . $_POST['search'] . "%";
+//     }
+//     $stmt->bindValue(':search', $bindstr);
+//   }
+//   $stmt->execute();
+//   $magicList = array();
+//   foreach ($stmt as $row){
+//     // 配列の順
+//     // (0)識別ID, (1)表示制限, (2)登場媒体, (3)名前, (4)属性, (5)属性英語名, (6)詠唱, (7)説明 
+//     $magicList[] = array(
+//       $row['id']
+//       , $row['netagard']
+//       , $row['media']
+//       , $row['name']
+//       , $row['type']
+//       , translateTypeJPtoEN($row['type'])
+//       , $row['chant']
+//       , $row['detail']
+//     );
+//   }
+// }
+$sql = "SELECT * FROM isesuma_word";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$wordList = array();
+foreach ($stmt as $row){
+  // 配列の順
+  // (0)識別ID, (1)表示制限, (2)登場媒体, (3)名前, (4)ルビ, (5)種別, (6)説明, (7)関連 
+  $wordList[] = array(
+    $row['id']
+    , $row['netagard']
+    , $row['media']
+    , $row['name']
+    , $row['ruby']
+    , $row['type']
+    , $row['detail']
+    , $row['relate']
+  );
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -94,7 +113,7 @@ if(!$pdo == null){
 <meta property="og:description" content="イセスマの世界をまるごと解説！人気急上昇作品の世界もこれで丸わかり！">
 <meta property="og:type" content="website">
 <meta property="og:image" content="http://isesuma.vallaria.net/img/top/main_img.jpg">
-<link rel="canonical" href="http://isesuma.vallaria.net/magic">
+<link rel="canonical" href="http://isesuma.vallaria.net/word">
 <!-- bootstrap -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -106,7 +125,7 @@ if(!$pdo == null){
 <!-- costum CSS -->
 <link rel="stylesheet" href="/css/common.css">
 <link rel="stylesheet" href="/css/modallist.css">
-<title>魔法一覧</title>
+<title>用語一覧</title>
 </head>
 <body>
   <header>
@@ -123,9 +142,9 @@ if(!$pdo == null){
   </header>
   <div class="bg-theme w-100 h-9px"></div>
   <main>
-    <section id="magic">
+    <section id="word">
       <h2 class="text-center">
-        <span>魔法一覧</span>
+        <span>用語一覧</span>
       </h2>
       <form method="post" id="magic_search_box" class="d-flex flex-wrap border mx-5 mt-4 mb-0 p-3">
         <div class="w-50 p-3 pb-0">
@@ -181,24 +200,25 @@ if(!$pdo == null){
         >※召喚魔法は除いております。
       </div>
       <div id="magic_list" class="mt-5">
-      <!-- (0)識別ID, (1)表示制限, (2)登場媒体, (3)名前, (4)属性, (5)属性英語名, (6)詠唱, (7)説明  -->
-        <?php $count = count($magicList);
+      <!-- (0)識別ID, (1)表示制限, (2)登場媒体, (3)名前, (4)ルビ, (5)種別, (6)説明, (7)関連  -->
+        <?php $count = count($wordList);
         for($i = 0; $i < $count; $i++){ ?>
-        <div id="magicItem_<?=$magicList[$i][0]?>" class="card" data-bs-toggle="modal" data-bs-target="#magicBackdrop_<?=$magicList[$i][0]?>"
-          ><span class="card-title fw-bold"><i class="bi-caret-right-fill pe-1"></i><?=$magicList[$i][3]?></span
-          ><span class="badge rounded-pill ms-1 bg-m-<?=$magicList[$i][5]?>"><?=$magicList[$i][4]?>属性</span
+        <div id="magicItem_<?=$wordList[$i][0]?>" class="card" data-bs-toggle="modal" data-bs-target="#magicBackdrop_<?=$wordList[$i][0]?>"
+          ><span class="card-title fw-bold"><i class="bi-caret-right-fill pe-1"></i><?=$wordList[$i][3]?></span
+          ><span class="badge rounded-pill ms-1 bg-media"><?=$wordList[$i][5]?></span
         ></div>
-        <div class="modal fade" id="magicBackdrop_<?=$magicList[$i][0]?>" data-bs-keyboard="true" tabindex="-1" aria-labelledby="magicBackdropLabel_<?=$magicList[$i][0]?>" aria-hidden="true">
+        <div class="modal fade" id="magicBackdrop_<?=$wordList[$i][0]?>" data-bs-keyboard="true" tabindex="-1" aria-labelledby="magicBackdropLabel_<?=$wordList[$i][0]?>" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title ps-2 pt-1 fs-4 fw-bold" id="magicBackdropLabel_<?=$magicList[$i][0]?>"><?=$magicList[$i][3]?></h5>
+                <h5 class="modal-title ps-2 pt-1 fs-4 fw-bold" id="magicBackdropLabel_<?=$wordList[$i][0]?>"><?=$wordList[$i][3]?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
-              <div class="modal-body pb-4">
-                <div class="magic_media pb-2"
+              <div class="modal-body pb-4"><?php if($wordList[$i][4] != ""){ ?>
+                <div class="magic_chant pb-2"><?=$wordList[$i][4]?></div><?php } ?>
+                <div class="magic_media"
                   <?php $mediaList = array(false, false, false, false, false);
-                  foreach(explode(",", $magicList[$i][2]) as $exp_media){
+                  foreach(explode(",", $wordList[$i][2]) as $exp_media){
                     switch($exp_media){
                       case "w":
                         $mediaList[0] = true;
@@ -222,47 +242,12 @@ if(!$pdo == null){
                   ><span class="badge rounded-pill me-1 bg-<?php $mediaList[3] == true ? print "media" : print "nonactive" ?>">アニメ1期</span
                   ><span class="badge rounded-pill me-1 bg-<?php $mediaList[4] == true ? print "media" : print "nonactive" ?>">アニメ2期</span
                 ></div>
-                <div class="magic_type pb-2"
-                  <?php $typeList = array(false, false, false, false, false, false, false);
-                  foreach(explode(",", $magicList[$i][4]) as $exp_media){
-                    switch($exp_media){
-                      case "火":
-                        $typeList[0] = true;
-                        break;
-                      case "水":
-                        $typeList[1] = true;
-                        break;
-                      case "土":
-                        $typeList[2] = true;
-                        break;
-                      case "風":
-                        $typeList[3] = true;
-                        break;
-                      case "光":
-                        $typeList[4] = true;
-                        break;
-                      case "闇":
-                        $typeList[5] = true;
-                        break;
-                      case "無":
-                        $typeList[6] = true;
-                    }
-                  } ?>
-                  ><span class="badge rounded-pill p-2 me-1 bg-<?php $typeList[0] == true ? print "m-fire" : print "nonactive" ?>">火</span
-                  ><span class="badge rounded-pill p-2 me-1 bg-<?php $typeList[1] == true ? print "m-water" : print "nonactive" ?>">水</span
-                  ><span class="badge rounded-pill p-2 me-1 bg-<?php $typeList[2] == true ? print "m-soil" : print "nonactive" ?>">土</span
-                  ><span class="badge rounded-pill p-2 me-1 bg-<?php $typeList[3] == true ? print "m-wind" : print "nonactive" ?>">風</span
-                  ><span class="badge rounded-pill p-2 me-1 bg-<?php $typeList[4] == true ? print "m-light" : print "nonactive" ?>">光</span
-                  ><span class="badge rounded-pill p-2 me-1 bg-<?php $typeList[5] == true ? print "m-dark" : print "nonactive" ?>">闇</span
-                  ><span class="badge rounded-pill p-2 me-1 bg-<?php $typeList[6] == true ? print "m-person" : print "nonactive" ?>">無</span
-                ></div><?php if($magicList[$i][6] != ""){ ?>
-                  <div class="magic_chant"><?=$magicList[$i][6]?></div><?php } ?>
                 <div class="row border-bottom mt-3 mb-4"></div>
-                <div class="px-4"><?=$magicList[$i][7]?></div>
+                <div class="px-4"><?=$wordList[$i][6]?></div>
               </div>
               <div class="modal-footer text-muted">
-                <?php if($i != 0){ ?><a style="font-size:0.95rem;" class="btn btn-outline-secondary ps-1 pe-3 py-1 me-2" data-bs-toggle="modal" data-bs-dismiss="modal" href="#magicBackdrop_<?=$magicList[$i-1][0]?>"><i class="bi-chevron-left pe-2"></i><?=$magicList[$i-1][3]?></a><?php } ?>
-                <?php if($i != $count - 1){ ?><a style="font-size:0.95rem;" class="btn btn-outline-secondary ps-3 pe-1 py-1" data-bs-toggle="modal" data-bs-dismiss="modal" href="#magicBackdrop_<?=$magicList[$i+1][0]?>"><?=$magicList[$i+1][3]?><i class="bi-chevron-right ps-2"></i></a><?php } ?>
+                <?php if($i != 0){ ?><a style="font-size:0.95rem;" class="btn btn-outline-secondary ps-1 pe-3 py-1 me-2" data-bs-toggle="modal" data-bs-dismiss="modal" href="#magicBackdrop_<?=$wordList[$i-1][0]?>"><i class="bi-chevron-left pe-2"></i><?=$wordList[$i-1][3]?></a><?php } ?>
+                <?php if($i != $count - 1){ ?><a style="font-size:0.95rem;" class="btn btn-outline-secondary ps-3 pe-1 py-1" data-bs-toggle="modal" data-bs-dismiss="modal" href="#magicBackdrop_<?=$wordList[$i+1][0]?>"><?=$wordList[$i+1][3]?><i class="bi-chevron-right ps-2"></i></a><?php } ?>
               </div>
             </div>
           </div>
